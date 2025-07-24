086f7713b409bf68a097a76074ea4201278a48c3
miromiro77 <carbon7727@gmail.com>
2025-07-24 12:04:27 +0900

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const jwt = require("jsonwebtoken");
 
-const applicationId = "YOUR_APPLICATION_ID"; // 여기에 본인 ID 넣기
-const privateKey = fs.readFileSync("./private.key");
+const applicationId = process.env.APPLICATION_ID;
+const privateKey = process.env.PRIVATE_KEY;  // fs.readFileSync 없이 바로
 
 const now = Math.floor(Date.now() / 1000);
 const payload = {
@@ -18,11 +18,12 @@ const payload = {
       "/image/**": {},
       "/media/**": {},
       "/messages/**": {},
-      "/knocking/**": {}
-    }
-  }
+      "/knocking/**": {},
+    },
+  },
 };
 
 const token = jwt.sign(payload, privateKey, { algorithm: "RS256" });
+
 fs.writeFileSync("jwt.txt", token);
 console.log("✅ JWT 생성 완료! → jwt.txt에 저장됨");
